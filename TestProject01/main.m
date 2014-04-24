019c6fcb3dcee550c88a5edc4bbc7044e2a6f978
salex5000 <githubprofile@gmail.com>
2014-04-24 01:46:04 -0700

    Initial commit from local git

---
+++ TestProject01/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  TestProject01
+//
+//  Created by salex on 4/24/14.
+//  Copyright (c) 2014 IAA. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
