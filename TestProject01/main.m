076b282ac59d0b6098310b64020bf2c4664bae41
salex5000 <githubprofile@gmail.com>
2014-04-24 23:24:24 -0700

    First commit to test project
    Completed the first tutorial about Objective-C

---
+++ TestProject01/main.m
@@ -0,0 +1,66 @@
+//
+//  main.m
+//  TestProject01
+//
+//  Created by salex on 4/24/14.
+//  Copyright (c) 2014 IAA. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+
+        int randomNum = arc4random() % 100 + 1;
+        int turn = 0;
+        int userInput = 0;
+        int initialGuess = -1;
+        
+        NSLog(@"Enter a number between 1 & 100");
+        @try{
+        scanf("%i", &userInput); //need to find a function that will pause and wait for the user to enter a new input.
+            //or that will throw and exception when a non-numeric value is entered.
+        }
+        @catch(NSException *e)
+        {
+            NSLog(@"%@", e.reason);
+            userInput = initialGuess;
+        }
+        turn++;
+        
+        //4NSLog(@"You entered: %i", userInput);
+        
+        while(userInput != randomNum)
+        {
+            @try{
+            if (userInput < randomNum)
+                NSLog(@"Guess higher");
+            else
+                NSLog(@"Guess lower");
+            
+            scanf("%i",&userInput);
+            
+            if(initialGuess == -1)
+            {
+                initialGuess = userInput;
+            }
+            
+            turn++;
+            }
+            @catch(NSException *e)
+            {
+                NSLog(@"%@",e.reason);
+                userInput = initialGuess;
+            }
+            
+        }
+        
+        NSLog(@"Yes! you guessed it right in %i turns!", turn);
+        
+        
+    }
+    return 0;
+}
+
